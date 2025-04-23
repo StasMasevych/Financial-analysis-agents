af404265dbf1e4d6e7d30a2d9d51c3e21b590774
Stas Masevych <109438310+StasMasevych@users.noreply.github.com>
2025-04-23 19:57:44 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Warning control
+import warnings
+warnings.filterwarnings('ignore')
+
+from crewai import Agent, Task, Crew
+
+import os
+from utils import get_openai_api_key, get_serper_api_key
+
+openai_api_key = get_openai_api_key()
+os.environ["OPENAI_MODEL_NAME"] = 'gpt-3.5-turbo'
+os.environ["SERPER_API_KEY"] = get_serper_api_key()
+
+from crewai_tools import ScrapeWebsiteTool, SerperDevTool
+
+search_tool = SerperDevTool()
+scrape_tool = ScrapeWebsiteTool()
