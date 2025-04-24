83bb04948e9a9809e352990ee2d0287e10c03ca6
Stas Masevych <109438310+StasMasevych@users.noreply.github.com>
2025-04-24 16:51:06 +0200

    add tasks and crew

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 import warnings
 warnings.filterwarnings('ignore')
 
-from crewai import Agent, Task, Crew
+from crewai import Agent, Task, Crew, Process
+from langchain_openai import ChatOpenAI
 
 import os
 from utils import get_openai_api_key, get_serper_api_key
@@ -74,3 +75,100 @@ risk_management_agent = Agent(
     allow_delegation=True,
     tools = [scrape_tool, search_tool]
 )
+
+
+# Create tasks
+
+# Task for Data Analyst Agent: Analyze Market Data
+data_analysis_task = Task(
+    description=(
+        "Continuously monitor and analyze market data for "
+        "the selected stock ({stock_selection}). "
+        "Use statistical modeling and machine learning to "
+        "identify trends and predict market movements."
+    ),
+    expected_output=(
+        "Insights and alerts about significant market "
+        "opportunities or threats for {stock_selection}."
+    ),
+    agent=data_analyst_agent,
+)
+
+# Task for Trading Strategy Agent: Develop Trading Strategies
+strategy_development_task = Task(
+    description=(
+        "Develop and refine trading strategies based on "
+        "the insights from the Data Analyst and "
+        "user-defined risk tolerance ({risk_tolerance}). "
+        "Consider trading preferences ({trading_strategy_preference})."
+    ),
+    expected_output=(
+        "A set of potential trading strategies for {stock_selection} "
+        "that align with the user's risk tolerance."
+    ),
+    agent=trading_strategy_agent,
+)
+
+# Task for Trade Advisor Agent: Plan Trade Execution
+execution_planning_task = Task(
+    description=(
+        "Analyze approved trading strategies to determine the "
+        "best execution methods for {stock_selection}, "
+        "considering current market conditions and optimal pricing."
+    ),
+    expected_output=(
+        "Detailed execution plans suggesting how and when to "
+        "execute trades for {stock_selection}."
+    ),
+    agent=execution_agent,
+)
+
+# Task for Risk Advisor Agent: Assess Trading Risks
+risk_assessment_task = Task(
+    description=(
+        "Evaluate the risks associated with the proposed trading "
+        "strategies and execution plans for {stock_selection}. "
+        "Provide a detailed analysis of potential risks "
+        "and suggest mitigation strategies."
+    ),
+    expected_output=(
+        "A comprehensive risk analysis report detailing potential "
+        "risks and mitigation recommendations for {stock_selection}."
+    ),
+    agent=risk_management_agent,
+)
+
+# Create crew   
+
+# Define the crew with agents and tasks
+financial_trading_crew = Crew(
+    agents=[data_analyst_agent, 
+            trading_strategy_agent, 
+            execution_agent, 
+            risk_management_agent],
+    
+    tasks=[data_analysis_task, 
+           strategy_development_task, 
+           execution_planning_task, 
+           risk_assessment_task],
+    
+    manager_llm=ChatOpenAI(model="gpt-3.5-turbo", 
+                           temperature=0.7),
+    process=Process.hierarchical,
+    verbose=True
+)
+
+# Example data for kicking off the process
+financial_trading_inputs = {
+    'stock_selection': 'AAPL',
+    'initial_capital': '100000',
+    'risk_tolerance': 'Medium',
+    'trading_strategy_preference': 'Day Trading',
+    'news_impact_consideration': True
+}
+
+### this execution will take some time to run
+# result = financial_trading_crew.kickoff(inputs=financial_trading_inputs)
+
+# print(result)
+
